67abc79929e72b192227c8248f93a72edb8a46d6
Vai-lla <igorkowaczyk24@gmail.com>
2022-04-04 23:53:27 +0200

    0.2

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -4,23 +4,23 @@ import './Nav.css'
 export default function Nav() {
     return (
         <nav>
-            <h1>
+            <h1 className='main-name'>
                 II Liceum Ogólno Kształcące im. Mikołaja Kopernika
             </h1>
             <div className='navigation'>
-                <a href='#'>
+                <a className='flash-text' href='#'>
                     <i className='fab fa-facebook-square'></i>
                 </a>
-                <a href='#'>
+                <a className='flash-text' href='#'>
                     <i className='fab fa-instagram'></i>
                 </a>
-                <a href='#'>
+                <a className='flash-text' href='#'>
                     <i className='fab fa-youtube'></i>
                 </a>
-                <a href='#'>
+                <a className='flash-text' href='#'>
                     <i className='fab fa-phone-square'></i>
                 </a>
-                <a href='#'>
+                <a className='flash-text' href='#'>
                     <i className='fab fa-chrome'></i>
                 </a>
             </div>
