e69269af038edc079bbd2c7eaf218830416d08cc
Vai-lla <igorkowaczyk24@gmail.com>
2022-04-04 22:22:27 +0200

    0.2

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import './Nav.css'
 
 export default function Nav() {
-  return (
-    <nav>
-        
-    </nav>
-  )
+    return (
+        <nav>
+            <h1>
+                II Liceum Ogólno Kształcące im. Mikołaja Kopernika
+            </h1>
+            <div className='navigation'>
+                <a href='#'>
+                    <i className='fab fa-facebook-square'></i>
+                </a>
+                <a href='#'>
+                    <i className='fab fa-instagram'></i>
+                </a>
+                <a href='#'>
+                    <i className='fab fa-youtube'></i>
+                </a>
+                <a href='#'>
+                    <i className='fab fa-phone-square'></i>
+                </a>
+                <a href='#'>
+                    <i className='fab fa-chrome'></i>
+                </a>
+            </div>
+        </nav>
+    )
 }
