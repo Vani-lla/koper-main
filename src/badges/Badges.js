4abb014bd1d37d61f98544a69f905e5cf912a16f
Vai-lla <igorkowaczyk24@gmail.com>
2022-04-07 23:19:29 +0200

    0.6 (better badges)

--- src/badges/Badges.js
+++ src/badges/Badges.js
@@ -2,17 +2,25 @@ import React from 'react'
 import logo from '../static/logo_new.png'
 import silver from '../static/sr22.png'
 import rekrut from '../static/rekrut.png'
+import dziennik from '../static/logo_dziennik.png'
+import erasmus from '../static/logo_erasmus.png'
 import './Badges.css'
 
 export default function Badges() {
     return (
         <div className='badges tile'>
-            <img id="main-logo" src={logo} alt='main logo'/>
+            <img id="main-logo" src={logo} alt='main logo' />
             <a href='https://licea.perspektywy.pl/2022/rankings'>
-                <img src={silver} alt='silver badge'/>
+                <img src={silver} alt='silver badge' />
             </a>
-            <a href='/rekrutacja'>
-                <img src={rekrut}/>
+            <a className='round-badge' href='/rekrutacja'>
+                <img src={rekrut} />
+            </a>
+            <a className='round-badge' href=''>
+                <img src={dziennik} />
+            </a>
+            <a className='round-badge' href=''>
+                <img src={erasmus} />
             </a>
         </div>
     )
